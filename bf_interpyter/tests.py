e461434bf618d3221f139122fe70cd70595aec72
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-05-02 21:33:10 +0200

    Moved to pytest framework

--- bf_interpyter/tests.py
+++ bf_interpyter/tests.py
@@ -1,23 +1,19 @@
-import unittest
+import pytest
 from bf_interpyter import get_bfvm_memory
 
-class TestInterpreterBehavior(unittest.TestCase):
-    def test_asking_for_N_size_returns_nparray_with_len_N(self):
-        N = 1000
-        memory = get_bfvm_memory(8, N)
-        self.assertEqual(len(memory), N)
+@pytest.mark.parametrize("N", [1, 23, 3849])
+def test_asking_for_N_size_returns_nparray_with_len_N(N):
+    memory = get_bfvm_memory(8, N)
+    assert len(memory) == N
 
-    def test_asking_for_minus_N_size_returns_empty_list(self):
-        N = -1000
-        memory = get_bfvm_memory(8, N)
-        self.assertEqual(len(memory), 0)
+@pytest.mark.parametrize("N", [0, -100, -248])
+def test_asking_for_minus_N_size_returns_empty_list(N):
+    memory = get_bfvm_memory(8, N)
+    assert len(memory) == 0
 
-    def test_asking_for_N_bit_number_returns_number_which_roll_after_overflow(self):
-        N = 8
-        memory = get_bfvm_memory(N, 1)
-        value_to_overflow = 2 ** (N - 1)
-        memory[0] += value_to_overflow
-        self.assertEqual(memory[0], -value_to_overflow)
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("N", [8])
+def test_asking_for_N_bit_number_returns_number_which_roll_after_overflow(N):
+    memory = get_bfvm_memory(N, 1)
+    value_to_overflow = 2 ** (N - 1)
+    memory[0] += value_to_overflow
+    assert memory[0] == -value_to_overflow
