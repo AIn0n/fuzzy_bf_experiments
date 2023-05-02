0e9e10f35410f58872752facff19f8d548d1d7ea
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-05-02 21:59:03 +0200

    Added red test with using plus command two times in row

--- bf_interpyter/tests.py
+++ bf_interpyter/tests.py
@@ -28,3 +28,11 @@ def test_execute_plus_N_times_sign_returns_N(N):
     interpreter = BF_interpreter()
     interpreter.execute("+" * N)
     assert interpreter.memory[0] == N
+
+@pytest.mark.parametrize("X,Y", [(1, 2), (8, 30), (21, 28)])
+def test_execute_plus_times_X_plus_times_Y_returns_XY(X, Y):
+    interpreter = BF_interpreter()
+    interpreter.execute("+" * X)
+    interpreter.execute("+" * Y)
+
+    assert interpreter.memory[0] == X + Y
